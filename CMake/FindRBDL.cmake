6d7b259618b10b838ef1cb4f8822ab3b18705df8
kjxymful <pleibersperger@yahoo.de>
2019-05-24 12:17:43 +0200

    updated stuff

--- CMake/FindRBDL.cmake
+++ CMake/FindRBDL.cmake
@@ -1,63 +1,21 @@
-# Searches for RBDL includes and library files
-#
-# Sets the variables
-#   RBDL_FOUND
-#   RBDL_INCLUDE_DIR
-#   RBDL_LIBRARY
+# Find the rbdl library.
+find_path(rbdl_INCLUDE_DIR rbdl.h /usr/include/rbdl /usr/local/include/rbdl)
 
+find_library(rbdl_LIBRARY
+    NAMES rbdl
+    PATHS /usr/lib /usr/local/lib
+) 
 
+if (rbdl_INCLUDE_DIR AND rbdl_LIBRARY)
+   set(rbdl_FOUND TRUE)
+endif (rbdl_INCLUDE_DIR AND rbdl_LIBRARY)
 
-
-SET (RBDL_FOUND FALSE)
-FIND_PATH (RBDL_INCLUDE_DIR rbdl/rbdl.h
-	/usr/include
-	/usr/local/include
-	$ENV{HOME}/local/include
-	$ENV{RBDL_PATH}/src
-	$ENV{RBDL_PATH}/include
-	$ENV{RBDL_INCLUDE_PATH}
-	${CMAKE_INSTALL_PREFIX}/include
-	)
-
-FIND_LIBRARY (RBDL_LIBRARY NAMES rbdl	PATHS
-	/usr/lib
-	/usr/local/lib
-	$ENV{HOME}/local/lib
-	$ENV{RBDL_PATH}
-	$ENV{RBDL_LIBRARY_PATH}
-	${CMAKE_INSTALL_PREFIX}/lib
-	)
-
-FIND_LIBRARY (RBDL_LUAMODEL_LIBRARY NAMES rbdl_luamodel	PATHS
-	/usr/lib
-	/usr/local/lib
-	$ENV{HOME}/local/lib
-	$ENV{RBDL_PATH}
-	$ENV{RBDL_LIBRARY_PATH}
-	${CMAKE_INSTALL_PREFIX}/lib
-	)
-
-IF (NOT RBDL_LUAMODEL_LIBRARY)
-	MESSAGE (ERROR "Could not find LuaModel Addon for RBDL")
-ENDIF (NOT RBDL_LUAMODEL_LIBRARY)
-
-IF (RBDL_INCLUDE_DIR AND RBDL_LIBRARY AND RBDL_LUAMODEL_LIBRARY)
-	SET (RBDL_FOUND TRUE)
-ENDIF (RBDL_INCLUDE_DIR AND RBDL_LIBRARY AND RBDL_LUAMODEL_LIBRARY)
-
-IF (RBDL_FOUND)
-   IF (NOT RBDL_FIND_QUIETLY)
-      MESSAGE(STATUS "Found RBDL: ${RBDL_LIBRARY}")
-			MESSAGE(STATUS "Found RBDL LuaModel: ${RBDL_LUAMODEL_LIBRARY}")
-   ENDIF (NOT RBDL_FIND_QUIETLY)
-ELSE (RBDL_FOUND)
-   IF (RBDL_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find RBDL")
-   ENDIF (RBDL_FIND_REQUIRED)
-ENDIF (RBDL_FOUND)
-
-MARK_AS_ADVANCED (
-	RBDL_INCLUDE_DIR
-	RBDL_LIBRARY
-	RBDL_LUAMODEL_LIBRARY
-	)
+if (rbdl_FOUND)
+  if (NOT rbdl_FIND_QUIETLY)
+    message(STATUS "Found rbdl: ${rbdl_LIBRARY}")
+  endif (NOT rbdl_FIND_QUIETLY)
+else (rbdl_FOUND)
+  if (rbdl_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find rbdl")
+  endif (rbdl_FIND_REQUIRED)
+endif (rbdl_FOUND)
--- src/integrator.cc
+++ src/integrator.cc
@@ -6,15 +6,10 @@
  *///=================================================================
 
 
-#include <string>
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <stdio.h> 
 #include <rbdl/rbdl.h>
-#include <rbdl/addons/luamodel/luamodel.h>
-#include <rbdl/addons/geometry/geometry.h>
-#include "csvtools.h"
+#include <Eigen/Core>
 
 #include <boost/numeric/odeint/stepper/runge_kutta_cash_karp54.hpp>
 #include <boost/numeric/odeint/stepper/controlled_runge_kutta.hpp>
@@ -35,11 +30,37 @@ typedef std::vector< double > state_type;
 typedef runge_kutta_cash_karp54< state_type > error_stepper_type;
 typedef controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
 
-std::string outLoc;
 
 //Multibody Variables
 Model* model;
 VectorNd q, qd, qdd, tau, x;  
+
+//Read a csv file
+template<typename M>
+M ReadCsv(const std::string& path) {
+    std::ifstream indata;
+    indata.open(path);
+    std::string line;
+    std::vector<double> values;
+    uint rows = 0;
+    while (std::getline(indata, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+        }
+        ++rows;
+    }
+    return Eigen::Map<const Eigen::Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, Eigen::RowMajor>>(values.data(), rows, values.size()/rows);
+}
+
+// Save data from Eigen matrix to .csv file.
+const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+template<typename M> 
+void WriteCsv(const std::string& path, M matrix){
+    std::ofstream file(path.c_str());
+    file << matrix.format(CSVFormat);
+};
 
 class RightHandSide {
 
@@ -72,9 +93,6 @@ class RightHandSide {
         ForwardDynamics(*model,q,qd,tau,qdd);
 
         //populate dxdt
-        //If you are using a quaternion joint, you must map wx,wy,wz to the 
-        //derivatives of the 4 quaternion components using the omegaToQDot 
-        //function
         j = 0;
         for(unsigned int i = 0; i < model->q_size; i++){
             dxdt[j] = double(qd[i]);
@@ -92,110 +110,128 @@ class RightHandSide {
 // Main.
 int main(int argc, char** argv) {
 
-  std::string fileName;
+  //load lua model
+  std::string filename;
+  filename= "/home/patrick/Documents/cartPenudlum_template/model.lua";
+  Addons::LuaModelReadFromFile(filename.c_str(), model);
 
-  // TODO lade pendulum.lua
-  if (!Addons::LuaModelReadFromFile(fileName.c_str(),model)){
-    std::cerr << "Error loading LuaModel: " << fileName << std::endl;
+  if (!Addons::LuaModelReadFromFile(filename.c_str(),model)){
+    std::cerr << "Error loading LuaModel: " << filename << std::endl;
     abort();
   }
 
-  // TODO lade meshup_cart_pendulum_count_0000.csv
-
+//load data in eigen matrix
+  std::string path = "/home/patrick/Documents/cartPenudlum_template/build/RES/meshup_cart_pendulum_count_0000.csv";
+  Eigen::MatrixXd data = ReadCsv<Eigen::MatrixXd>(path);
 
   // TODO for loop über alle shooting notes
-  // for ()
-  // {
+  int r = data.rows();
+  int c = data.cols();
+
+  // std::vector<std::vector< double > > matrixData;
+  Eigen::MatrixXd matrixData(0, c);
+  // std::vector< double > rowData(model->dof_count+1);
+  Eigen::MatrixXd rowData(1, model->dof_count+1);
+
+  for (int i = 0; i<x; i++){
+
+    Eigen::MatrixXd m(r,c-1);
+    Eigen::MatrixXd qm(r,c-1);
+    Eigen::MatrixXd qdm1(r,c-1);
+    Eigen::MatrixXd qdm(r,c-1);
+    Eigen::MatrixXd taum(r,c-1);
+
+    m=data.rightCols(c-1);
+    qm=m.leftCols(2);
+    qdm1=m.rightCols(3);
+    qdm=qdm1.leftCols(1);
+    taum=m.rightCols(1);
 
     q.resize(model->dof_count);
     qd.resize(model->dof_count);
     tau.resize(model->dof_count); // tau[0] = u
 
-    // TODO Anfangswerte setzen, dh q, qd, tau (Werte aus meshup_cart_pendulum_count_0000.csv)
-    // q = ...
-    // qd = ...
-    // tau = ...
+      // TODO Anfangswerte setzen, dh q, qd, tau (Werte aus meshup_cart_pendulum_count_0000.csv)
+    q = qm.row(i).matrix();
+    qd = qdm.row(i).matrix();
+    tau = taum.row(i).matrix();
 
     x.resize(model->dof_count*2);
     for(unsigned int i=0; i<q.rows();++i){
-      x[i] =q[i];
-      x[i+q.rows()] = qd[i];
-    }
+        x[i] =q[i];
+        x[i+q.rows()] = qd[i];
 
-    RightHandSide rhs();
+
+    RightHandSide rhs;
 
     state_type xState(x.size());
     state_type dxState(x.size());
     for(unsigned int i=0; i<x.size(); ++i){
       xState[i]   = x[i];
-    }
-
-    // TODO Anfangswerte setze, dh t0 und t1 (Werte aus meshup_cart_pendulum_count_0000.csv)
-    double t;
-    double t0; 
-    double t1;
-    unsigned int npts      = 100;
-
-    // t0 = ...
-    // t1 = ...
-
-    double absTolVal = 1e-8;
-    double relTolVal = 1e-8;
-
-    double dt = (t1-t0)/(npts-1);
-    unsigned int k=0;
-
-    std::vector<std::vector< double > > matrixData;
-    std::vector< double > rowData(model->dof_count+1);
-
-    double a_x = 1.0 , a_dxdt = 1.0;
-    controlled_stepper_type
-    controlled_stepper(
-        default_error_checker< double ,
-                              range_algebra ,
-                              default_operations >
-        ( absTolVal , relTolVal , a_x , a_dxdt ) );
-
-    double tp = 0;
-    rowData[0] = 0;
-    for(unsigned int z=0; z < model->dof_count; z++){
-        rowData[z] = xState[z];
-    }
-    matrixData.push_back(rowData);
-
-    for(unsigned int i=0; i<= npts; ++i){
-      t = t0 + dt*i;
-
-      integrate_adaptive(
-          controlled_stepper ,
-          rhs , xState , tp , t , (t-tp)/10 );
-      tp = t;
-
-
-      for(unsigned int j=0; j<x.rows();++j){
-        x[j] = xState[j];
-      }
-      k=0;
-      for(unsigned int j=0; j<model->q_size;++j){
-        q[j] = xState[k];
-        ++k;
-      }
-      for(unsigned int j=0; j<model->qdot_size;++j){
-        qd[j] = xState[k];
-        ++k;
       }
 
+      // TODO Anfangswerte setze, dh t0 und t1 (Werte aus meshup_cart_pendulum_count_0000.csv)
+      double t; //t?
+      double t0=data(0,i); 
+      double t1=data(0,i+1);
+      unsigned int npts      = 100;
 
-      rowData[0] = t;
+
+      double absTolVal = 1e-8;
+      double relTolVal = 1e-8;
+
+      double dt = (t1-t0)/(npts-1);
+      unsigned int k=0;
+
+      double a_x = 1.0 , a_dxdt = 1.0;
+      controlled_stepper_type
+      controlled_stepper(
+          default_error_checker< double ,
+                                range_algebra ,
+                                default_operations >
+          ( absTolVal , relTolVal , a_x , a_dxdt ) );
+
+      double tp = 0;
+      rowData[0] = 0;
       for(unsigned int z=0; z < model->dof_count; z++){
-          rowData[z+1] = xState[z];
+          rowData[z] = xState[z];
+      }
+      // matrixData.push_back(rowData);
+      matrixData.conservativeResize(matrixData.rows()+1, Eigen::NoChange_t);
+      matrixData.bottomRows(1) = rowData;
+
+      for(unsigned int i=0; i<= npts; ++i){
+        t = t0 + dt*i;
+
+        integrate_adaptive(
+            controlled_stepper ,
+            rhs , xState , tp , t , (t-tp)/10 );
+        tp = t;
+
+        for(unsigned int j=0; j<x.rows();++j){
+          x[j] = xState[j];
+        }
+        k=0;
+        for(unsigned int j=0; j<model->q_size;++j){
+          q[j] = xState[k];
+          ++k;
+        }
+        for(unsigned int j=0; j<model->qdot_size;++j){
+          qd[j] = xState[k];
+          ++k;
+        }
+
+        rowData[0] = t;
+        for(unsigned int z=0; z < model->dof_count; z++){
+            rowData[z+1] = xState[z];
+        }
+        matrixData.conservativeResize(matrixData.rows()+1, Eigen::NoChange_t);
+        matrixData.bottomRows(1) = rowData;
       }
     }
-  // }
+  }
+  std::string file_path = "/home/patrick/Documents/integrator/Data/integrated_data_0000.csv";
+  WriteCsv(file_path, matrixData);
 
-    std::string emptyHeader("");
-    std::string fileNameOut(outLoc + ".csv");
-    printMatrixToFile(matrixData,emptyHeader,fileNameOut);
-
-   return 0; 
+  return 0; 
 }
